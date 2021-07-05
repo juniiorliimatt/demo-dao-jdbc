ce4cc91b38393260d0b894d737e994a1011c023d
Junior Lima <juniiorliimatt@gmail.com>
2021-07-05 16:19:57 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
-import model.entities.Department;
-import model.entities.Seller;
-
-import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program{
   public static void main(String[] args){
-    var department = new Department(1,"Books");
-    var seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,department);
+
+    SellerDao sellerDao = DaoFactory.createSellerDao();
+    var seller = sellerDao.findById(18);
     System.out.println(seller);
   }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -20,8 +20,16 @@ public class Department implements Serializable{
     return id;
   }
 
+  public void setId(Integer id){
+    this.id = id;
+  }
+
   public String getName(){
     return name;
+  }
+
+  public void setName(String name){
+    this.name = name;
   }
 
   @Override
